6519551b8d46e3fe3d424b9531eda583877de849
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2022-05-24 14:48:19 -0300

    prepare Enemy for Test

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -1,2 +1,9 @@
 class Enemy < ApplicationRecord
+  enum kind: %i[goblin orc demon dragon]
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
+  validates_presence_of :name, :kind, :power_base, :power_step, :level
+
+  def current_power
+    power_base + ((level - 1) * power_step)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index create]
   resources :weapons
+  resources :enemies, only: %i[update destroy]
 end
